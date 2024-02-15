1edb9fda12e37075916b72bac5e4b3819aa2d698
luigirinaldi <luigirinaldi@users.noreply.github.com>
2024-02-15 18:27:05 +0000

    cleanup: use sub because quicker and get rid of debug comments

--- hello_world/software/count_binary/count_binary.c
+++ hello_world/software/count_binary/count_binary.c
@@ -74,37 +74,25 @@ float sumVector(float x[0], int M)
 // constant coefficient division
 const float coeff1 = 0.5, coeff2 = 1 / 128.0f, coeff3 = 128.0f;
 // taylor series terms 
-const float c_term1 = 1/2.0, c_term2 = 1 / 24.0f, c_term3 = 1/40320.0;
+const float c_term1 = 1/2.0, c_term2 = 1 / 24.0f;
 
 float theFunction(float x[0], int M) {
   float sum = 0;
   int i = 0;
   for (; i < M; i++) 
   {
-    const float diff =  x[i] - 128.0f;
+    const float diff =  x[i] + -128.0f;
 
-    // const __uint32_t float_int = *(int*)&diff;
-    // const __uint32_t new_exp = ((*(int*)&diff >> 23) & 0xff) - 7;
     const __uint32_t new_float = DividePow2(diff, 7);
     const float cos_term = *(float*)&new_float;
-  //   float actual_cos = diff * coeff2;
 
-  // if (actual_cos != cos_term) {
-
-  //     printf("original n: %x %f\n", float_int, diff );
-  //     printf("custom div: %x %f exp: %u %u \n", new_float, cos_term, new_exp, (float_int >> 23) & 0xff);
-  //     printf("real div:   %x %f exp: %u \n", *(int*)&actual_cos, actual_cos,  ((*(int*)&actual_cos) >> 23) & 0xff);
-  // }
-    // const float cos_term = diff * coeff2;
     const float cos_2 = cos_term * cos_term;
     const float cos_4 = cos_2 * cos_2;
-    // const float cos_6 = cos_4 * cos_2;
 
      __uint32_t term1_int =  DividePow2(cos_2, 1);
-     term1_int |= 0x80000000;
     const float term1 =  *(float*)&term1_int;
 
-    const float cosine = 1 + term1  + cos_4 * c_term2;
+    const float cosine = 1 - term1  + cos_4 * c_term2;
     
     sum += (coeff1 * x[i] + (x[i] * x[i]) * cosine);
   }
